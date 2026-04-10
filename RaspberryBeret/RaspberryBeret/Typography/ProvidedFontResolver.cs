c6db514a623d515c7a13e9d24d3233c7c33d0aca
DESKTOP-LT9URFC\byron <byron@byronwjones.com>
2026-04-10 18:29:06 -0400

    Create typography service for providing fonts

---
+++ RaspberryBeret/RaspberryBeret/Typography/ProvidedFontResolver.cs
@@ -0,0 +1,78 @@
+﻿using PdfSharp.Fonts;
+
+namespace RaspberryBeret.Typography;
+
+/// <summary>
+/// Resolves fonts using provided resources
+/// </summary>
+/// <see cref="https://stackoverflow.com/questions/27606877/pdfsharp-private-fonts-for-azure-1-50"/>
+class ProvidedFontResolver : IFontResolver
+{
+    public FontResolverInfo? ResolveTypeface(string familyName, bool isBold, bool isItalic)
+    {
+        var name = familyName.ToLower().Split('#')[0];
+        var fontService = TypographyService.GetTypeface(name);
+        if (fontService is not null)
+        {
+            if (isBold)
+            {
+                if (isItalic)
+                {
+                    return new FontResolverInfo($"{fontService.Name}#bi");
+                }
+                return new FontResolverInfo($"{fontService.Name}#b");
+            }
+            if (isItalic)
+            {
+                return new FontResolverInfo($"{fontService.Name}#i");
+            }
+            return new FontResolverInfo($"{fontService.Name}#");
+        }
+
+        // Try to resolve fonts not provided on the local machine
+        return PlatformFontResolver.ResolveTypeface(familyName, isBold, isItalic);
+    }
+
+    public byte[]? GetFont(string faceName)
+    {
+        var name = faceName.ToLower().Split('#')[0];
+        var fontService = TypographyService.GetTypeface(name);
+        if (fontService is not null)
+        {
+            if(faceName == $"{fontService.Name}#b")
+            {
+                return fontService.GetBold();
+            }
+            else if (faceName == $"{fontService.Name}#i")
+            {
+                return fontService.GetItalic();
+            }
+            else if (faceName == $"{fontService.Name}#bi")
+            {
+                return fontService.GetBoldItalic();
+            }
+            else
+            {
+                return fontService.GetNormal();
+            }
+        }
+
+        return null;
+    }
+
+    internal static ProvidedFontResolver? GlobalFontResolver = null;
+
+    /// <summary>
+    /// Register this as the font resolver
+    /// </summary>
+    internal static void Apply()
+    {
+        if (GlobalFontResolver == null || GlobalFontSettings.FontResolver == null)
+        {
+            if (GlobalFontResolver == null)
+                GlobalFontResolver = new ProvidedFontResolver();
+
+            GlobalFontSettings.FontResolver = GlobalFontResolver;
+        }
+    }
+}
---
+++ RaspberryBeret/RaspberryBeret/Typography/TypographyService.cs
@@ -0,0 +1,58 @@
+﻿using BWJ.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RaspberryBeret.Typography;
+internal static class TypographyService
+{
+    public static void RegisterTypeface(ITypefaceService typefaceService)
+    {
+        MethodGuard.NoNull(new { typefaceService });
+        if (Stringy.Empty(typefaceService.Name))
+        {
+            throw new Exception("Font name must be provided on typeface service");
+        }
+
+        lock(@lock)
+        {
+            var key = typefaceService.Name.ToLower().Trim();
+            if(typefaces.ContainsKey(key))
+            {
+                typefaces[key] = typefaceService;
+            }
+            else
+            {
+                typefaces.Add(key, typefaceService);
+            }
+        }
+    }
+
+    public static ITypefaceService? GetTypeface(string fontName)
+    {
+        MethodGuard.NoEmptyString(new { fontName });
+
+        ITypefaceService? typefaceService = null;
+        lock (@lock)
+        {
+            var key = fontName.ToLower().Trim();
+            if(typefaces.ContainsKey(key))
+            {
+                typefaceService = typefaces[key];
+            }
+        }
+
+        return typefaceService;
+    }
+
+    public static bool InUse()
+    {
+        lock(@lock)
+        {
+            return typefaces.Any();
+        }
+    }
+
+    private static readonly Dictionary<string, ITypefaceService> typefaces = new();
+    private static object @lock = new();
+}
